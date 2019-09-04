c0ba89418208c1831999e9af4f3a482aedd5cad1
paul wishropp <pwishropp@gmail.com>
2019-09-04 22:43:09 +0000

    added modals

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,16 +1,18 @@
 class PhotosController < ApplicationController
-  before_action :authenticate_user!
-
- def create
-  @place = Place.find(params[:place_id])
-  @place.photos.create(photo_params.merge(user: current_user))
-  redirect_to place_path(@place)
- end
-
- private
-
- def photo_params
-  params.require(:photo).permit(:caption, :picture)
- end
-end
-
+    before_action :authenticate_user!
+  
+   def create
+    @place = Place.find(params[:place_id])
+    @place.photos.create(photo_params.merge(user: current_user))
+   if @place.invalid?
+      flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+   end
+    redirect_to place_path(@place)
+   end
+  
+   private
+  
+   def photo_params
+    params.require(:photo).permit(:caption, :picture)
+   end
+  end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,6 @@ class Photo < ApplicationRecord
   belongs_to :place
   belongs_to :user
   mount_uploader :picture, PictureUploader
+  validates :caption, presence: true, length: { maximum: 140, minimum: 3}
+  validates :picture, presence: true
 end
