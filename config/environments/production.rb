71bc730e6de22b24aba121f0be7c5f48aeaf2fcd
paul wishropp <pwishropp@gmail.com>
2019-09-06 12:49:59 +0000

    added carousel

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
 
-  config.action_mailer.default_url_options = { host: 'nomster-paul-wishropp.herokuapp.com' }
+  # config.action_mailer.default_url_options = { host: 'nomster-paul-wishropp.herokuapp.com' }
 
   # Settings specified here will take precedence over those in config/application.rb.
 
